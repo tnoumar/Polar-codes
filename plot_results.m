bd2e7bd927f719adeca786a3b8e610f6483a1868
Taha <taha.noumar@bordeaux-inp.fr>
2021-10-30 15:28:58 +0200

    main.m code refactor

--- plot_results.m
+++ plot_results.m
@@ -2,10 +2,10 @@ close all
 
 % figure 1 is for BER plotting
 figure(1)
-% load NC
+ load 6_3.mat
 hold on 
 for i=1:length
-semilogy(EbN0dB,BER(4,:));
+semilogy(EbN0dB,FER);
 
 xlim([-2 10])
 ylim([1e-6 1])
